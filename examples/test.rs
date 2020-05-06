b91842593168a0ece5309427ae2ca5f8c4de6e5f
Rousan Ali <rousanali786@gmail.com>
2020-05-06 22:28:02 +0530

    Rename test files

--- examples/test.rs
+++ examples/test.rs
@@ -108,7 +108,7 @@ async fn main() {
     let service = RouterService::new(router).unwrap();
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let server = Server::bind(&addr).serve(router_service);
+    let server = Server::bind(&addr).serve(service);
 
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
