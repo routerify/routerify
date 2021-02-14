53ab75d4d75c9148b7e47abd00cac5f51ecd08b0
Sergey Glushchenko <gsserge@gmail.com>
2021-02-14 11:14:52 +0100

    Add request context

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,7 +1,9 @@
+pub(crate) use request_context::RequestContext;
 pub use request_info::RequestInfo;
 pub(crate) use request_meta::RequestMeta;
 pub use route_params::RouteParams;
 
+mod request_context;
 mod request_info;
 mod request_meta;
 mod route_params;
