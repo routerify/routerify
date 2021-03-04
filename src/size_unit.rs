f78d1b276669e334c55d91153934a945a3e7426c
BuildTools <wolfkingboyasriel@gmail.com>
2021-03-04 05:34:26 -0600

    Reword documentation

--- src/size_unit.rs
+++ src/size_unit.rs
@@ -1,4 +1,4 @@
-/// An Enum used to specify a unit's size. Used for the [max_size](./struct.RouterBuilder.html#method.max_size) function.
+/// An Enum used for specifying a data type's size. Used for the [max_size](./struct.RouterBuilder.html#method.max_size) function.
 pub enum SizeUnit {
     Byte,
     Kilo,
